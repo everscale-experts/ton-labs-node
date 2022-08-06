2e45e681463da0b1194a0a419f7f7e192bb36d56
JavaharlalN <grechanichenkos@yandex.ru>
2022-08-06 10:43:44 +0300

    Add tests and fix `check_file_and_write_message`

--- writer/src/lib.rs
+++ writer/src/lib.rs
@@ -1,23 +1,92 @@
 use std::fmt::Display;
 
-pub fn check_file_and_write_message<T: Display>(flag_path: &str, description: &str, message: &T) {
-	if let Ok(counter_str) = std::fs::read_to_string(format!("{}", flag_path)) {
-		let mut counter: u8 = match counter_str.parse() {
+pub fn check_file_and_write_message<T: Display>(
+	flag_path: &str, // path without start.txt
+	description: &str,
+	message: &T
+) {
+	let flag_file_path = &format!("../{}/start.txt", flag_path);
+	if let Ok(counter_str) = std::fs::read_to_string(flag_file_path) {
+		let mut counter: usize = match counter_str.parse() {
 			Ok(u) => u,
 			Err(_) => 0,
 		};
 		if counter_str == "" { counter = 9; }
 		if counter == 0 {
-			std::fs::remove_file(flag_path).ok();
+			std::fs::remove_file(flag_file_path).ok();
 		} else {
 			std::fs::write(
-				format!("{}/message{}.txt", flag_path, 9 - counter),
+				// format!("{}/message{}.txt", flag_path, 9 - counter),
+				format!("../{}/message{}.txt", flag_path, counter),
 				format!("{}: {}", description, message)
 			).ok();
 			std::fs::write(
-				flag_path,
+				flag_file_path,
 				format!("{}", counter - 1)
 			).ok();
 		}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use std::io::Write;
+	use std::fs;
+	use crate::check_file_and_write_message;
+	const FLAG_PATH: &str = "../../debugLog/start.txt";
+	
+	#[test]
+	fn test_without_file() {
+		fs::remove_file(FLAG_PATH).ok();
+		let res_before = fs::read_to_string(FLAG_PATH);
+		check_file_and_write_message("../debugLog", "", &String::new());
+		let res_after = fs::read_to_string(FLAG_PATH);
+		assert!(res_before.is_err() && res_after.is_err());
+		assert!(match res_after {
+			Ok(_) => false,
+			Err(_) => true
+		});
+	}
+
+	#[test]
+	fn test_empty() {
+		fn clear_file(create: bool) {
+			let mut file = if create {
+				fs::create_dir("../../debugLog").ok();
+				fs::File::create(FLAG_PATH)
+			} else {
+				fs::File::open(FLAG_PATH)
+			}.unwrap();
+			file.write_all(b"").unwrap();
+		}
+		match fs::read_to_string(FLAG_PATH) {
+			Ok(v) => if v != "" { clear_file(false) },
+			Err(_) => clear_file(true)
+		}
+		assert!(std::path::Path::new(FLAG_PATH).exists());
+		check_file_and_write_message("../debugLog", "", &String::new());
+		let counter = std::fs::read_to_string(FLAG_PATH).ok().unwrap_or("".to_string());
+		assert!(counter.parse::<usize>().is_ok() || counter == "", "file content is not a number: '{}'", counter);
+	}
+
+	#[test]
+	fn test_with_value() {
+		fn write_value(v: usize) -> usize {
+			fs::create_dir("../../debugLog").ok();
+			let mut file = fs::File::create(FLAG_PATH).unwrap();
+			file.write(format!("{}", v).as_bytes()).unwrap();
+			v
+		}
+
+		let counter = match fs::read_to_string(FLAG_PATH) {
+			Ok(v) => v.parse().ok().unwrap_or(write_value(15)),
+			Err(_) => write_value(15),
+		};
+
+		for _ in 0..counter + 1 {
+			check_file_and_write_message("../debugLog", "", &String::new());
+		}
+
+		assert!(!std::path::Path::new(FLAG_PATH).exists(), "file wasn`t deleted");
 	}
 }
