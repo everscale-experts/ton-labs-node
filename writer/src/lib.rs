7ad64812696ca06615c3eb5f1377ab16ee57cf5a
JavaharlalN <grechanichenkos@yandex.ru>
2022-08-09 15:15:42 +0300

    Update tests and writer crate

--- writer/src/lib.rs
+++ writer/src/lib.rs
@@ -11,34 +11,34 @@ pub static mut COUNTER: usize = 0;
 pub fn listen_flag_file() {
     unsafe {
         if DEBUGLOG_PATH.is_empty() {
-            set_path(Some("./debugLog".to_string()));
+            set_default_debuglog_path();
         }
     }
-	loop {
-		std::thread::sleep(std::time::Duration::from_millis(WAIT_TIMEOUT_MS));
-		if let Ok(counter_str) = std::fs::read_to_string(flag_path()) {
-			unsafe {
-				COUNTER = match counter_str.parse() {
-					Ok(u) => u,
-					Err(_) => 0,
-				};
-				if counter_str == "" { COUNTER = 9; }
-				std::fs::remove_file(flag_path()).ok();
-			}
-		}
-	}
+    loop {
+        std::thread::sleep(std::time::Duration::from_millis(WAIT_TIMEOUT_MS));
+        if let Ok(counter_str) = std::fs::read_to_string(flag_path()) {
+            unsafe {
+                COUNTER = match counter_str.parse() {
+                    Ok(u) => u,
+                    Err(_) => 0,
+                };
+                if counter_str == "" { COUNTER = 9; }
+                std::fs::remove_file(flag_path()).ok();
+            }
+        }
+    }
 }
 
 pub fn write_message<T: Display>(description: &str, message: &T) {
-	unsafe {
-		if COUNTER > 0 {
-			std::fs::write(
-				format!("../{}/message{}.txt", DEBUGLOG_PATH, COUNTER),
-				format!("{}: {}", description, message)
-			).ok();
-			COUNTER -= 1;
-		}
-	}
+    unsafe {
+        if COUNTER > 0 {
+            std::fs::write(
+                format!("../{}/message{}.txt", DEBUGLOG_PATH, COUNTER),
+                format!("{}: {}", description, message)
+            ).ok();
+            COUNTER -= 1;
+        }
+    }
 }
 
 pub(crate) fn flag_path() -> String {
@@ -52,5 +52,11 @@ pub fn set_path(debuglog_path: Option<String>) {
         unsafe {
             DEBUGLOG_PATH = path.clone();
         }
+    }
+}
+
+pub fn set_default_debuglog_path() {
+    unsafe {
+        DEBUGLOG_PATH = String::from("./debugLog");
     }
 }
--- writer/src/test.rs
+++ writer/src/test.rs
@@ -4,45 +4,67 @@ use std::fs;
 use std::time::Duration;
 use crate::*;
 
+/// Creates start.txt and writes a string ("wrong data") there,
+/// then it simulates a message writing,
+/// compares COUNTER value with expected value (0)
+/// and ensures that start.txt has been deleted.
+/// 
+/// It is for ensure that the listener is
+/// deletes flag file after discovering unexpected value there.
 #[test]
 fn test_listener_with_wrong_data() {
-	thread::sleep(Duration::from_millis(WAIT_TIMEOUT_MS * 2 + 2000));
-	fs::remove_file(flag_path()).ok();
-	std::thread::spawn(move || {
-		listen_flag_file();
-	});
-	let mut file = fs::File::create(flag_path()).unwrap();
-	file.write_all(b"wrong data").unwrap();
-	thread::sleep(Duration::from_millis(WAIT_TIMEOUT_MS + 1000));
-	assert_eq!(unsafe { COUNTER }, 0);
-	write_message("test listener with wrong data", &String::new());
-	assert!(!std::path::Path::new(&flag_path()).exists(), "file wasn`t deleted");
+    thread::sleep(Duration::from_millis(WAIT_TIMEOUT_MS * 2 + 2000));
+    fs::remove_file(flag_path()).ok();
+    std::thread::spawn(move || {
+        listen_flag_file();
+    });
+    let mut file = fs::File::create(flag_path()).unwrap();
+    file.write_all(b"wrong data").unwrap();
+    thread::sleep(Duration::from_millis(WAIT_TIMEOUT_MS + 1000));
+    assert_eq!(unsafe { COUNTER }, 0);
+    write_message("test listener with wrong data", &String::new());
+    assert!(!std::path::Path::new(&flag_path()).exists(), "file wasn`t deleted");
 }
 
+/// Creates empty start.txt,
+/// then it simulates a message writing
+/// and compares COUNTER value with expected value (8).
+/// 
+/// It is for ensure that the default value is
+/// correctly writes to the COUNTER.
 #[test]
 fn test_listener_with_empty_flag_file() {
-	thread::sleep(Duration::from_millis(WAIT_TIMEOUT_MS + 1500));
-	fs::remove_file(flag_path()).ok();
-	std::thread::spawn(move || {
-		listen_flag_file();
-	});
-	let mut file = fs::File::create(flag_path()).unwrap();
-	file.write_all(b"").unwrap();
-	thread::sleep(Duration::from_millis(WAIT_TIMEOUT_MS + 1000));
-	write_message("test listener with empty flag file", &String::new());
-	assert_eq!(unsafe { COUNTER }, 8);
+    thread::sleep(Duration::from_millis(WAIT_TIMEOUT_MS + 1500));
+    fs::remove_file(flag_path()).ok();
+    std::thread::spawn(move || {
+        listen_flag_file();
+    });
+    let mut file = fs::File::create(flag_path()).unwrap();
+    file.write_all(b"").unwrap();
+    thread::sleep(Duration::from_millis(WAIT_TIMEOUT_MS + 1000));
+    write_message("test listener with empty flag file", &String::new());
+    assert_eq!(unsafe { COUNTER }, 8);
 }
 
+/// Creates start.txt and writes 5 there,
+/// then it simulates a message writing
+/// and compares COUNTER value with expected value (4).
+///
+/// It is for ensure that the value is correctly
+/// reads from start.txt and writes to the COUNTER.
 #[test]
 fn test_listener_with_value() {
-	fs::remove_file(flag_path()).ok();
-	std::thread::spawn(move || {
-		listen_flag_file();
-	});
-
-	let mut file = fs::File::create(flag_path()).unwrap();
-	file.write_all(b"5").unwrap();
-	thread::sleep(Duration::from_millis(WAIT_TIMEOUT_MS + 1000));
-	write_message("test listener with value", &String::new());
-	assert_eq!(unsafe { COUNTER }, 4);
+    fs::remove_file(flag_path()).ok();
+    set_default_debuglog_path();
+    std::thread::spawn(move || {
+        listen_flag_file();
+    });
+    unsafe {
+        fs::create_dir_all(&DEBUGLOG_PATH).ok();
+    }
+    let mut file = fs::File::create(flag_path()).unwrap();
+    file.write_all(b"5").unwrap();
+    thread::sleep(Duration::from_millis(WAIT_TIMEOUT_MS + 1000));
+    write_message("test listener with value", &String::new());
+    assert_eq!(unsafe { COUNTER }, 4);
 }
